0137e13bd829b3c693e31619eb931293e795408e
Mickael Faivre-Macon <faivrem@gmail.com>
2015-06-26 23:21:14 +0200

    sorting ended games

--- client/templates/opengames/opengames.js
+++ client/templates/opengames/opengames.js
@@ -13,7 +13,7 @@ Template.openGames.helpers({
   },
 
   'endedGames': function() {
-    return Games.find({status: 'ended'}, {sort: {createdAt: 1}});
+    return Games.find({status: 'ended'}, {sort: {lastMovedAt: -1}});
   }
 });
 
